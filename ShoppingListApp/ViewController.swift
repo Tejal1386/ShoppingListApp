21a28dfec075b28185835823b1ca9b853511f45e
Tejal1386 <tejalmca1386@gmail.com>
2017-12-06 21:28:58 -0500

    final commit

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -1,25 +1,77 @@
-//
-//  ViewController.swift
-//  ShoppingListApp
-//
-//  Created by Tejal Patel on 2017-12-06.
-//  Copyright © 2017 Centennial College. All rights reserved.
-//
-
+/*
+ FileName = ShoppingListApp
+ Aouther's Name = Tejal Patel
+ Student_ID = 300972812
+ Date = 06/12/2017
+ */
 import UIKit
 
-class ViewController: UIViewController {
-
+// UIviewcontroller class inherits UIPickerViewDelegate,UIPickerViewDataSource
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    ///////////////////outlets///////////////////////
+    @IBOutlet weak var textname: UITextField!
+    @IBOutlet weak var quantity: UITextField!
+    @IBOutlet weak var textitem: UITextField!
+    
+    @IBOutlet weak var textItem1: UITextField!
+    @IBOutlet weak var textItem2: UITextField!
+    @IBOutlet weak var textItem3: UITextField!
+    @IBOutlet weak var textItem4: UITextField!
+    @IBOutlet weak var textItem5: UITextField!
+    
+    @IBOutlet weak var Pic_item1: UIPickerView!
+    @IBOutlet weak var Pic_item2: UIPickerView!
+    @IBOutlet weak var Pic_item3: UIPickerView!
+    @IBOutlet weak var Pic_item4: UIPickerView!
+    @IBOutlet weak var Pic_item5: UIPickerView!
+    private let characterNames = [
+        "1", "2", "3", "4", "5",
+        "6", "7","8","9","10"]
+    
+    
+    //////////////////////////////////////////////////////////
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    /// Reset Button : Reset all control values////
+    @IBAction func reset(_ sender: UIButton) {
+        
+        textname.text = ""
+        textitem.text = ""
+        quantity.text = ""
+        textItem1.text = ""
+        textItem2.text = ""
+        textItem3.text = ""
+        textItem4.text = ""
+        textItem5.text = ""
+        self.Pic_item1.selectRow(0, inComponent: 0, animated: true)
+        self.Pic_item2.selectRow(0, inComponent: 0, animated: true)
+        self.Pic_item3.selectRow(0, inComponent: 0, animated: true)
+        self.Pic_item4.selectRow(0, inComponent: 0, animated: true)
+        self.Pic_item5.selectRow(0, inComponent: 0, animated: true)
+        
     }
-
-
+    
+    // pickerview datasource method to show number of components in pickerview
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // pickerview datasource method that count number of components in pickerview
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count
+    }
+    
+    // Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component:
+        Int) -> String? {
+        return characterNames[row]
+    }
+    
 }
 
